28a5120201d35d465a5668fc1a90a974f5138cae
lz <lz980213@163.com>
2020-01-12 10:54:09 +0800

    the first homework

---
+++ 0112-strat/python_foundation.py
@@ -0,0 +1,36 @@
+'''
+    (第一部分，数据类型和变量)
+    Please print the following vaules
+    n = 123
+    f = 456.789
+    s1 = 'Hello, world'
+    s2 = 'Hello, \'Adam\''
+    s3 = r'Hello, "Bart"'
+    s4 = r'''Fool,yuqing!'''
+
+
+    (第二部分，字符串)
+    小明的成绩从去年的72分提升到了今年的85分，请计算小明成绩提升的百分点，并用字符串格式化显示出'xx.x%'，只保留小数点后1位：
+
+'''
+
+def main():
+
+
+    print("############# First section：###########")
+    '''
+       Finish by yourself!
+    '''
+
+
+    print("############# Second section：###########")
+    s1 = 72
+    s2 = 85
+    # r = ?
+    # print("???", % r)
+    '''
+       Finish by yourself!
+    '''
+
+if __name__ = "__main__":
+    main()
