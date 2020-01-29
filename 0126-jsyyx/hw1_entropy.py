edc57f1e44a1bb5064f56dc40b10e76b15d464be
yuqing0304 <yuqingelsa@gmail.com>
2020-01-29 14:41:03 +0800

    add hw1_entropy

--- 0126-jsyyx/hw1_entropy.py
+++ 0126-jsyyx/hw1_entropy.py
@@ -38,9 +38,11 @@ def main():
     w1, f1, e1 = get_freq_entropy("../data/littleprince/text1.txt")
     w2, f2, e2 = get_freq_entropy("../data/littleprince/text2.txt")
     kl_f1_f2 = get_kl(w1, f1, w2, f2)
+    kl_f2_f1 = get_kl(w2, f2, w1, f1)
     print("text1 的 entropy 为: {}".format(e1))
     print("text2 的 entropy 为: {}".format(e2))
     print("text1 和 text2 的 KL divergence 是：{}".format(kl_f1_f2))
+    print("text2 和 text1 的 KL divergence 是：{}".format(kl_f2_f1))
 
 
 if __name__ == "__main__":
